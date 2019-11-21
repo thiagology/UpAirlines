ed00f5188daf461228985c83b07c2233b7eccd45
EDSON OLIVEIRA <45743063+Suntragan@users.noreply.github.com>
2019-11-21 08:37:33 -0300

    5

--- src/projetoUp/model/Passagem.java
+++ src/projetoUp/model/Passagem.java
@@ -4,12 +4,14 @@ public class Passagem {
 	private String codigo;
 	private Passageiro passageiro;
 	private Voo voo;
+	private float preco;
 	
 	public Passagem(String codigo, Passageiro passageiro, Voo voo) {
 		super();
 		this.codigo = codigo;
 		this.passageiro = passageiro;
 		this.voo = voo;
+		this.setPreco();
 	}
 
 	public String getCodigo() {
@@ -34,6 +36,53 @@ public class Passagem {
 
 	public void setVoo(Voo voo) {
 		this.voo = voo;
+	}
+	
+	public void setPreco()
+	{
+		float tPreco;
+		
+		if(this.voo.getDistancia() <= 200 && this.voo.getDistancia() > 0)
+		{
+			tPreco = 200.00f;
+		}
+		
+		else if(this.voo.getDistancia() > 200 && this.voo.getDistancia() <= 500)
+		{
+			tPreco = 450f;
+		}
+		
+		else if(this.voo.getDistancia() > 500 && this.voo.getDistancia() <= 800)
+		{
+			tPreco = 650f;
+		}
+		
+		else if(this.voo.getDistancia() > 800 && this.voo.getDistancia() <= 1500)
+		{
+			tPreco = 900f;
+		}
+		
+		else if(this.voo.getDistancia() > 1500 && this.voo.getDistancia() <= 2500)
+		{
+			tPreco = 1300f;
+		}
+		
+		else if(this.voo.getDistancia() > 2500 && this.voo.getDistancia() <= 4000)
+		{
+			tPreco = 1500f;
+		}
+		
+		else
+		{
+			tPreco = 2500f;
+		}
+		
+		this.preco = tPreco;
+	}
+	
+	public float getPreco()
+	{
+		return this.preco;
 	}
 	
 	
