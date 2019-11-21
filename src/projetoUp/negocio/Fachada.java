a7f32c64540c1205ab75a5a1e3e99baa468be5bf
thiagology <46846692+thiagology@users.noreply.github.com>
2019-11-20 23:53:56 -0300

    atlz

--- src/projetoUp/negocio/Fachada.java
+++ src/projetoUp/negocio/Fachada.java
@@ -1,5 +1,11 @@
 package projetoUp.negocio;
 
+import java.util.List;
+
+import projetoUp.model.Passageiro;
+import projetoUp.model.Passagem;
+import projetoUp.model.Voo;
+
 public class Fachada {
 	private static Fachada instance;
 	
@@ -24,6 +30,49 @@ public class Fachada {
 	
 
 	//gerente
+	public void criarVoo(Voo v) {
+		controladorVoo.criarVoo(v);
+	}
+
+	public void removerVoo(Voo v) {
+		controladorVoo.removerVoo(v);
+	}
+
+	public void alterarVoo(Voo v) {
+		controladorVoo.alterarVoo(v);
+	}
+
+	public void listar() {
+		controladorVoo.listar();
+	}
+
+	public void buscarVoo(String codigo) {
+		controladorVoo.buscarVoo(codigo);
+	}
+
+	public void buscarPassagem(String c) {
+		controladorPassagem.buscarPassagem(c);
+	}
+	
+	
+	
+	
 	
 	//cliente
+	
+	public void criarPassagem(Passagem p) {
+		controladorPassagem.criarPassagem(p);
+	}
+
+	public void excluirPassagem(Passagem p) {
+		controladorPassagem.excluirPassagem(p);
+	}
+
+	public void alterarPassagem(Passagem p) {
+		controladorPassagem.alterarPassagem(p);
+	}
+
+	public List<Passagem> listarPorPassageiro(Passageiro pa) {
+		return controladorPassagem.listarPorPassageiro(pa);
+	}
 }
