9aadb20f4f022c30654967b71cfb598bdf742e9c
EDSON OLIVEIRA <45743063+Suntragan@users.noreply.github.com>
2019-11-25 20:04:42 -0300

    f

--- src/projetoUp/ClasseTesteDados.java
+++ src/projetoUp/ClasseTesteDados.java
@@ -3,13 +3,9 @@ package projetoUp;
 import java.time.LocalDate;
 
 import projetoUp.model.Endereco;
-import projetoUp.model.Gerente;
-import projetoUp.negocio.ControladorAeroporto;
 import projetoUp.negocio.Fachada;
 
 public class ClasseTesteDados {
-	
-	
 	
 
 	public static void main(String[] args) {
@@ -18,8 +14,8 @@ public class ClasseTesteDados {
 		//Novo Gerente
 		Endereco endereco = new Endereco("Recife", "Pernambuco", "Brasil", 54500000);
 		
+	
 		
-		fachada.adicionarFuncionario("Gerente", "1234", "4321", 81823400, endereco, LocalDate.of(1992, 5, 10), 123, LocalDate.of(2019, 3, 15), "Gerente");
 		
 		
 	}
