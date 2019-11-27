52ef003e111589e90f13fc6481b98730a69e4372
thiagology <46846692+thiagology@users.noreply.github.com>
2019-11-27 00:33:04 -0300

    telas add

--- src/projetoUp/gui/TelaGerenteController.java
+++ src/projetoUp/gui/TelaGerenteController.java
@@ -1,5 +1,54 @@
 package projetoUp.gui;
 
-public class TelaGerenteController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+
+public class TelaGerenteController implements Initializable {
+
+    @FXML
+    private Tab tbVoos;
+
+    @FXML
+    private Button btBuscarVoo;
+
+    @FXML
+    private Button btAlterarVoo;
+
+    @FXML
+    private Button btRemoverVoo;
+
+    @FXML
+    private Button btInserirVoo;
+
+    @FXML
+    private Tab tbFunc;
+
+    @FXML
+    private Button btInserirFunc;
+
+    @FXML
+    private Button btBuscarFunc;
+
+    @FXML
+    private Button btRemoverFunc;
+    
+
+    @FXML
+    void InserirVoo(ActionEvent event) {
+
+    }
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/projetoUp/gui/TelaMainController.java
+++ src/projetoUp/gui/TelaMainController.java
@@ -1,9 +1,28 @@
 package projetoUp.gui;
 
-public class TelaMainController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class TelaMainController extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Pane mainPane =(Pane) FXMLLoader.load(TelaMainController.class.getResource("TelaGerente.fxml"));
+        
+        Scene scene = new Scene(mainPane);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Application.launch(args);
 
 	}
 
