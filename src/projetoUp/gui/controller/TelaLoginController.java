0e5f1a6713fed438f9dd032212f45740219ebf6c
thiagology <46846692+thiagology@users.noreply.github.com>
2019-12-01 17:06:06 -0300

    tela login

--- src/projetoUp/gui/controller/TelaLoginController.java
+++ src/projetoUp/gui/controller/TelaLoginController.java
@@ -3,11 +3,14 @@ package projetoUp.gui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import projetoUp.negocio.Fachada;
 
 public class TelaLoginController {
 
@@ -37,10 +40,45 @@ public class TelaLoginController {
 
     @FXML
     void onClickLogin(ActionEvent event) {
-    	//IMPLEMENTAR:
-    	//se o login existe
-    	//se é o gerente ou um cliente
-    	//definir telas especificas
+    	String msgErro = "";
+		if(txLogin.getText() == null ||
+			txSenha.getText() == null) {
+			msgErro = "Login ou senha inválidos";
+		}
+		boolean isLog = Fachada.getInstance().fazerLogin(txLogin.getText(),
+														 txSenha.getText());
+    	if(isLog == false) {
+    		msgErro = "Usuário inexistente";
+    	}
+    	if (msgErro.length() == 0) {
+    		Stage dialog = new Stage();
+    		if(txLogin.getText() == "Gerente" &&
+    		   txSenha.getText() == "senha") {
+    			
+    	        dialog.setScene(ScreenManager.getInstance().getGerenteScene());
+    	        dialog.setResizable(false);
+    	        dialog.setTitle("Criar Conta");
+    	        dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+    	        dialog.initModality(Modality.APPLICATION_MODAL);
+    	        dialog.showAndWait();
+    		}
+    		else {
+    	        dialog.setScene(ScreenManager.getInstance().getClienteScene());
+    	        dialog.setResizable(false);
+    	        dialog.setTitle("Criar Conta");
+    	        dialog.initOwner(((Node) event.getSource()).getScene().getWindow());
+    	        dialog.initModality(Modality.APPLICATION_MODAL);
+    	        dialog.showAndWait();
+    		}
+    		
+    	}else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Erro");
+            alert.setHeaderText(msgErro);
+            alert.setContentText("");
+
+            alert.showAndWait();
+        }
     	
     }
 }
